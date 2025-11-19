caa91a612c02bada735550820d3eaf9d810f5a4f
Maluleke-Khensani <165706638+Maluleke-Khensani@users.noreply.github.com>
2025-11-19 18:22:30 +0200

    Initialize in-memory database for claim system
    Switched form SQL Lite to SQL Sever Database
    Set up the in-memory database configuration required for storing lecturer claims. Added DbContext, claim models, and initial service structure to prepare for application workflows.

---
+++ PRO6212_CLaim_System_MVC/Claims_System/Areas/Identity/Data/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Claims_System.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Claims_System.Data;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<LecturerClaim> LecturerClaims { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<LecturerClaim>()
+        .Property(c => c.HoursWorked)
+        .HasPrecision(18, 2); // 18 digits, 2 decimal places
+
+    builder.Entity<LecturerClaim>()
+        .Property(c => c.Rate)
+        .HasPrecision(18, 2);
+
+    base.OnModelCreating(builder);
+    }
+}
